e9ecd6cc44280f4924bdbb68094c4a0e7bf54a98
Sandeep Pandita <sandeeppandita46@gmail.com>
2018-10-08 16:49:15 +0530

    SocketIO Basic Added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
-
+const server = http.createServer(app);
+var io = socketIO(server);
+io.on('connection', socket => {
+    console.log("New Client Connected");
+    
+    socket.on('disconnect', socket =>{
+        console.log("Client Disconnected");
+    })
+})
 app.use(express.static(publicPath));
-
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Chat Server is listening on port: ${port}`);
 })
